1f33dce01d223025c5bd1dc038afb445de481b81
JohnnyMohawk <johnnelsonalden@gmail.com>
2021-08-23 08:47:10 -0500

    pre troubleshoot field.field bug

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Challenge
+from .models import Challenge, People
 # Register your models here.
 admin.site.register(Challenge)
+admin.site.register(People)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+PEOPLE_TYPES = (
+    ('L', 'Person to learn about'),
+    ('C', 'Person to reach out to'),
+    ('G', 'Person I am grateful for'),
+)
+
 # Create your models here.
 class Challenge(models.Model):
     inspiration = models.CharField(max_length=100)
@@ -11,3 +17,15 @@ class Challenge(models.Model):
 
     def get_absolute_url(self):
         return reverse('challenges_detail', kwargs={'challenge_id': self.id})
+
+class People(models.Model):
+    type = models.CharField(
+        max_length=1,
+        choices=PEOPLE_TYPES,
+        default=PEOPLE_TYPES[0][0]
+    )
+    name = models.CharField(max_length=50)
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
+
+    # def __str__(self):
+    #     return f"{self.get_type_display()} on {self.date}"
